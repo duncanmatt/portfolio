1d201634254ddc3020f2d6ca9232db5f56699f48
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-02-13 13:08:41 -0500

    theme and removed logo from footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,17 +1,11 @@
-import Initials from '../layout/assets/initials.svg'
-
 function Footer() {
-  const footerYear = new Date().getFullYear();
+	const footerYear = new Date().getFullYear();
 
-  return (
-    <div className='footer'>
-      <img src={Initials} alt='MD' style={{
-        alignSelf: 'center',
-        width: '66px'
-      }} />
-      <p>Copyright &copy; {footerYear} All rights reserved</p>
-    </div>
-  )
+	return (
+		<div className='footer'>
+			<p>Copyright &copy; {footerYear} All rights reserved</p>
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
