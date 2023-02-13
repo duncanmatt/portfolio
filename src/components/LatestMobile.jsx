cab0a964211c77accb00904deac567bb93bb99b6
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-02-12 20:57:16 -0500

    updated items array to include url
    should have left a comment

--- src/components/LatestMobile.jsx
+++ src/components/LatestMobile.jsx
@@ -1,9 +1,52 @@
-import Carousel from './shared/Carousel';
+import { useMediaQuery } from "react-responsive";
+import bswPreview from '../layout/assets/bswPreview.png';
+import ghFinderPreview from '../layout/assets/ghFinderPreview.png';
+import fbAppPreview from '../layout/assets/fbAppPreview.png';
+import hmPreview from '../layout/assets/hmPreview.png';
+import SmCarousel from "./SmCarousel";
+import LgCarousel from "./LgCarousel";
 
 function LatestMobile() {
+	const isSmall = useMediaQuery({ query: '(max-width: 400px)' });
+
+	const items = [
+		{
+			id: 1,
+			title: 'GITHUB FINDER',
+			desc: 'React app that gives users the ability to search for and view profiles on GitHub by making use of the GitHub API.',
+			pic: `${ghFinderPreview}`,
+			url: 'https://github-finder-rho-ivory.vercel.app/',
+		},
+		{
+			id: 2,
+			title: 'ECOMMERCE SITE',
+			desc: 'Website made using the MERN stack for my friend in the process of starting a clothing brand.',
+			pic: `${bswPreview}`,
+			url: 'https://blacksheepworld.herokuapp.com',
+		},
+		{
+			id: 3,
+			title: 'FEEDBACK APP',
+			desc: 'React app for leaving reviews that are stored with firebase',
+			pic: `${fbAppPreview}`,
+			url: 'https://feedback-app-seven-mauve.vercel.app/',
+		},
+		{
+			id: 4,
+			title: 'HOUSE MARKETPLACE',
+			desc: 'React app for posting or viewing real estate listings that are stored with firebase',
+			pic: `${hmPreview}`,
+			url: 'https://house-marketplace-nu-ochre.vercel.app/',
+		},
+	];
+
 	return (
 		<div className='carouselWrapper'>
-			<Carousel />
+			{isSmall ? (
+				<SmCarousel items={items} />
+			) : (
+				<LgCarousel items={items} />
+			)}
 		</div>
 	);
 }
---
+++ src/components/LgCarousel.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import ProjectCard from './ProjectCard';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+
+function LgCarousel({ items }) {
+	const [activeItem, setActiveItem] = useState(1);
+
+	const nextItem = () => {
+		setActiveItem(activeItem === items.length ? 1 : activeItem + 1);
+	};
+
+	const prevItem = () => {
+		setActiveItem(activeItem === 1 ? items.length : activeItem - 1);
+	};
+
+	return (
+		<div className='carouselLarge'>
+			<h2>Latest</h2>
+			<div className='carousel'>
+				<button
+					className='carouselPrev'
+					onClick={prevItem}>
+					<LeftOutlined />
+				</button>
+				<div className='carouselInner'>
+					{items.map(item => (
+						<div
+							key={item.id}
+							className={
+								item.id !== activeItem ? 'carouselItem' : 'carouselItemActive'
+							}>
+							<ProjectCard project={item} />
+						</div>
+					))}
+				</div>
+				<button
+					className='carouselNext'
+					onClick={nextItem}>
+					<RightOutlined />
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default LgCarousel;
