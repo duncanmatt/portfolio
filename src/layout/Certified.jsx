d15ff959877b6877d10db7b4094e31a39f0d43ac
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-02-24 21:23:04 -0500

    theme

--- src/layout/Certified.jsx
+++ src/layout/Certified.jsx
@@ -1,4 +1,4 @@
-import { useMediaQuery } from 'react-responsive';
+import { Divider } from 'antd';
 import DSAPic from '../layout/assets/dsa.jpg';
 import ReactPic from '../layout/assets/reactCert.jpg';
 import BeginnerJSPic from '../layout/assets/beginner-js-cert.png';
@@ -6,27 +6,26 @@ import Certifications from '../components/Certifications';
 
 function Certified() {
 	let certs = [
-    {
-      id: 1,
-      pic: DSAPic,
-      desc: 'Took this course from Colt Steele after getting comfortable with JavaScript.'
-    }, 
-    {
-      id: 2,
-      pic: BeginnerJSPic,
-      desc: 'Was inspired to learn a programming language after having fun playing around with HTML/CSS.'
-    },
-    {
+		{
+			id: 1,
+			pic: DSAPic,
+			desc: 'Took this course from Colt Steele after getting comfortable with JavaScript.',
+		},
+		{
+			id: 2,
+			pic: BeginnerJSPic,
+			desc: 'Was inspired to learn a programming language after having fun playing around with HTML/CSS.',
+		},
+		{
 			id: 3,
 			pic: ReactPic,
 			desc: 'A great course from Brad Traversy, one of my favorite content creators. I would highly recommend to anyone wanting to start building modern apps.',
 		},
-  ];
-	// const isMobile = useMediaQuery({ query: '(max-width: 600px)' });
+	];
 
 	return (
 		<div className='certified'>
-			<h2>Certified</h2>
+      <h2>Certified</h2>
 			<Certifications certs={certs} />
 		</div>
 	);
